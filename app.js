1fab21cdee574ca1f2837001e9fd28e795c4b8b3
hashrock <hashedrock@gmail.com>
2015-10-16 18:31:13 +0900

    Add view engine setting to avoid error

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hjs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
